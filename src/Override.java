c4c6f05bbbe64630f2623aa2b732c227d7588957
KingJargielo <lukasz.jargielo1988@gmail.com>
2022-03-23 08:49:46 +0100

    Przesłanianie metod i dziedziczenie

--- src/Override.java
+++ src/Override.java
@@ -5,10 +5,28 @@ class A5 {
         i = a;
         j = b;
     }
+
+    void show() {
+        System.out.println("i and j: " + i + " " + j);
+    }
 }
 
-void
+class B5 extends A5 {
+    int k;
 
+    B5 (int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+
+    void show() {
+        System.out.println("k: " + k);
+    }
+}
 public class Override {
+    public static void main(String[] args) {
+        B5 subObj = new B5(1, 2, 3);
 
+        subObj.show();
+    }
 }

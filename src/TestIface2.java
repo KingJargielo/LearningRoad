afabcbd028fdf94a5034a7913c695611b6ad6993
KingJargielo <lukasz.jargielo1988@gmail.com>
2022-03-31 10:55:57 +0200

    Interfejsy cd

---
+++ src/TestIface2.java
@@ -0,0 +1,11 @@
+public class TestIface2 {
+    public static void main(String[] args) {
+        Callback c = new Client();
+        AnotherClient obj = new AnotherClient();
+
+        c.callback(42);
+
+        c = obj;
+        c.callback(42);
+    }
+}

8eb37e13201ab8c42a02780125f1da5ada28d6f4
KingJargielo <lukasz.jargielo1988@gmail.com>
2022-03-31 17:08:48 +0200

    Metody prywatne w interfejsach

--- src/IntStack.java
+++ src/IntStack.java
@@ -4,5 +4,24 @@ interface IntStack {
 
     default void clear() {
         System.out.println("The Clear() method has not been implemented.");
+
     }
+        default int[] popNElements(int n) {
+            return getElements(n);
+        }
+
+        default int[] skipAndPopElements(int skip, int n) {
+
+            getElements(skip);
+
+            return getElements(n);
+        }
+
+        private int[] getElements(int n) {
+            int[] elements = new int[n];
+            for(int i=0; i < n; i++) elements[i] = pop();
+            return elements;
+        }
+
+
 }
